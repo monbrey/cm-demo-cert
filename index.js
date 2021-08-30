3935def6070db6b0d98a0c73ad6a7d19e7f8cb87
Ryan Munro <Ryan.Munro@uts.edu.au>
2021-08-30 16:04:24 +1000

    fix: remove config

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const initCACCL = require('caccl/server/react');
 
 // Initialize CACCL
 const app = initCACCL({
-	disableServerSideAPI: true,
 	installationCredentials: {
 		consumer_key: "cert",
 		consumer_secret: "cert"
